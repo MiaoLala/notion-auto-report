a5051194aee1d59e3c573b22cbed2b73d8d9fc02
MiaoLala <111435584+MiaoLala@users.noreply.github.com>
2025-06-21 01:07:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ database_id = "2182a91a-405d-80fe-82eb-c3bf47bfe625"
 
 # LINE Messaging API Token & userId
 LINE_ACCESS_TOKEN = os.environ["LINE_ACCESS_TOKEN"]
-LINE_USER_ID = "miaolala_" # os.environ["LINE_USER_ID"]  # 你要發訊息的對象（自己或用戶）
+LINE_USER_ID = "Ueac062fbefdeffa4bc3a4020db58fff6" # os.environ["LINE_USER_ID"]  # 你要發訊息的對象（自己或用戶）
 
 def send_line_message(user_id, message):
     url = "https://api.line.me/v2/bot/message/push"
